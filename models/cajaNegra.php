cb722d3c8cf0dafac0d6c93a06fbb19eaabf9526
fran8narf <srlk_doblev@hotmail.com>
2016-10-29 16:54:55 +0200

    classes created

---
+++ models/cajaNegra.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cajaNegra
+ *
+ * @author Fran
+ */
+class cajaNegra {
+    //put your code here
+}
---
+++ models/cajaSorpresa.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of cajaSorpresa
+ *
+ * @author Fran
+ */
+class cajaSorpresa {
+    //put your code here
+}
