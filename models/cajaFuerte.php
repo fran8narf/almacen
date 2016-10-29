9706e444a1df258a7581077b5c44846410027374
fran8narf <srlk_doblev@hotmail.com>
2016-10-29 17:02:33 +0200

    cajaFuerte-constructor, getter y setter creado.

--- models/cajaFuerte.php
+++ models/cajaFuerte.php
@@ -12,5 +12,80 @@
  * @author Fran
  */
 class cajaFuerte {
-    //put your code here
+    private $ID;
+    private $color;
+    private $altura;
+    private $anchura;
+    private $profundidad;
+    private $clave_desbloqueo;
+    private $fecha_registro;
+    
+    
+    public function __construct($ID, $color, $altura, $anchura, $profundidad, $clave_desbloqueo) {
+        $this->ID = $ID;
+        $this->color = $color;
+        $this->altura = $altura;
+        $this->anchura = $anchura;
+        $this->profundidad = $profundidad;
+        $this->clave_desbloqueo = $clave_desbloqueo;
+        $this->fecha_registro = new Date();
+    }
+    
+    public function getID() {
+        return $this->ID;
+    }
+
+    public function getColor() {
+        return $this->color;
+    }
+
+    public function getAltura() {
+        return $this->altura;
+    }
+
+    public function getAnchura() {
+        return $this->anchura;
+    }
+
+    public function getProfundidad() {
+        return $this->profundidad;
+    }
+
+    public function getClave_desbloqueo() {
+        return $this->clave_desbloqueo;
+    }
+
+    public function getFecha_registro() {
+        return $this->fecha_registro;
+    }
+
+    public function setID($ID) {
+        $this->ID = $ID;
+    }
+
+    public function setColor($color) {
+        $this->color = $color;
+    }
+
+    public function setAltura($altura) {
+        $this->altura = $altura;
+    }
+
+    public function setAnchura($anchura) {
+        $this->anchura = $anchura;
+    }
+
+    public function setProfundidad($profundidad) {
+        $this->profundidad = $profundidad;
+    }
+
+    public function setClave_desbloqueo($clave_desbloqueo) {
+        $this->clave_desbloqueo = $clave_desbloqueo;
+    }
+
+    public function setFecha_registro($fecha_registro) {
+        $this->fecha_registro = $fecha_registro;
+    }
+
+
 }
